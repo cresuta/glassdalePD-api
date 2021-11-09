3cad5ec85fe24d1da809c1df8d4a6a99fba22eff
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-09 15:58:15 -0500

    working on saveNote feature on edit form, id vvalue for editedNote obj needs changed

--- src/scripts/notes/NoteEditForm.js
+++ src/scripts/notes/NoteEditForm.js
@@ -3,6 +3,17 @@ import {useNotes} from "./NoteDataProvider.js"
 // We're going to print the edit form where the "add note" form usually goes. We could move it around on the page by changing our content target.
 const contentTarget = document.querySelector(".noteFormContainer")
 
+contentTarget.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("saveNote")) {
+        const editedNote = {
+            id: 1,
+            date: document.querySelector('#note-date').value,
+            suspect: document.querySelector('#note-suspect').value,
+            text: document.querySelector('#note-text').value
+        }
+    }
+})
+
 export const NoteEditForm = (noteId) => {
     // Give this component access to our application's notes state
     const allNotes = useNotes();
@@ -13,11 +24,11 @@ export const NoteEditForm = (noteId) => {
     // Print the form
     // We'll use the HTML value attribute to pre-populate our form fields with the note's info
     contentTarget.innerHTML = `
-        <h2>Edit Note</h2>
-        <form class="row g-3">
+    <div class="edit-form"> 
+        <h2 class="editNote-heading">Edit Note</h2>     
   <div class="col-md-12">
     <label for="noteDate" class="form-label">Date</label>
-    <input type="date" name="noteDate" class="form-control" id="note-date" value="${noteWeWantToEdit.date}">
+    <input type="date" name="noteDate" class="form-control" id="note-date" value="${(noteWeWantToEdit.date)}">
   </div>
   <div class="col-md-12">
     <label for="noteSuspect" class="form-label">Suspect In Question:</label>
@@ -25,12 +36,12 @@ export const NoteEditForm = (noteId) => {
   </div>
   <div class="col-12">
     <label for="noteText" class="form-label">Notes:</label>
-    <input type="text" value="${noteWeWantToEdit.text}" id="note-suspect" />
+    <input type="text" class="form-control" name="noteText" value="${noteWeWantToEdit.text}" id="note-suspect" />
   </div>
   <div class="col-12">
     <button type="submit" class="btn btn-light btn-outline-dark" id="saveNote-${noteId}">
     <i class="bi bi-journal-check"></i>Save Note</button>
   </div>
-</form>
+    </div>
     `
 }
