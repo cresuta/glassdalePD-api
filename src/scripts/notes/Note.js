4c2a2b8e2718d1712ed61264e0330f761380f04a
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-15 15:56:49 -0500

    changed delete/edit btn to icons without text and gave event listeners to both button and icon element

--- src/scripts/notes/Note.js
+++ src/scripts/notes/Note.js
@@ -4,7 +4,7 @@ import {NoteEditForm} from "./NoteEditForm.js"
 
 // Click listener for delete button
 document.querySelector('.current-list').addEventListener("click", clickEvent => {
-    if(clickEvent.target.id.startsWith("deleteNote")) {
+    if(clickEvent.target.id.startsWith("deleteNote") || clickEvent.target.id.startsWith("deleteBtn")) {
         const idToDelete = clickEvent.target.id.split("-")[1];
         deleteNote(idToDelete)
         .then(NoteList)
@@ -13,7 +13,7 @@ document.querySelector('.current-list').addEventListener("click", clickEvent => 
 
 // Click listener for edit button
 document.querySelector('.current-list').addEventListener("click", clickEvent => {
-    if(clickEvent.target.id.startsWith("editNote")) {
+    if(clickEvent.target.id.startsWith("editNote") || clickEvent.target.id.startsWith("editBtn")) {
         const idToEdit = +clickEvent.target.id.split("-")[1];
         NoteEditForm(idToEdit)
     }
@@ -21,19 +21,18 @@ document.querySelector('.current-list').addEventListener("click", clickEvent => 
 
 export const Note = (note,criminal) => {
     return `
-        <div class="card" style="width: 20rem">
+        <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 20rem">
         <div class="card-body">
-            <i class="bi bi-journal-text"></i>
             <h5 class="card-title">${new Date(note.date).toLocaleDateString('en-US')}</h5>
-            <p class="card-text">Suspect In Question - ${criminal.name}</p>
-            <p class="card-text">${note.text}</p>
+            <p class="card-text card-criminalName">${criminal.name}</p>
+            <p class="card-text card-noteText">${note.text}</p>
         </div>
         <div class="card-btns">
             <button type="submit" class="btn btn-light btn-outline-dark note-btn" id="deleteNote-${note.id}">
-                <i class="bi bi-trash"></i>Delete Note</button>
+                <i class="bi bi-trash" id="deleteBtn-${note.id}"></i></button>
             
             <button type="submit" class="btn btn-light btn-outline-dark note-btn" id="editNote-${note.id}">
-                <i class="bi bi-pencil-square"></i>Edit Note</button>
+                <i class="bi bi-pencil-square" id="editBtn-${note.id}"></i></button>
         </div>
         </div>
     `
