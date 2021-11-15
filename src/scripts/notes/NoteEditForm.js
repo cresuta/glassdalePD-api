925666f059f43e0a29f9d0171ad5621a4942f010
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-15 14:41:57 -0500

    edit note btn not autopopulating criminal name in suspect in question dropdown, but all other input fields are editing correctly

--- src/scripts/notes/NoteEditForm.js
+++ src/scripts/notes/NoteEditForm.js
@@ -1,5 +1,7 @@
-import {useNotes, updateNote} from "./NoteDataProvider.js"
+import {useNotes, updateNote, getNotes} from "./NoteDataProvider.js"
 import { NoteList } from "./NoteList.js"
+import { getCriminals, useCriminals } from "../criminals/CriminalDataProvider.js"
+import { Note } from "./Note.js"
 
 const contentTarget = document.querySelector(".noteFormContainer")
 
@@ -9,7 +11,7 @@ contentTarget.addEventListener("click", clickEvent => {
       const editedNote = {
             id: parseInt(clickEvent.target.id.split("-")[1]),
             date: document.querySelector('#note-date').value,
-            suspect: document.querySelector('#note-suspect').value,
+            criminalId: parseInt(document.querySelector('#note-suspect').value),
             text: document.querySelector('#note-text').value
         }
 
@@ -18,7 +20,7 @@ contentTarget.addEventListener("click", clickEvent => {
     }
 })
 
-export const NoteEditForm = (noteId) => {
+ /* export const NoteEditForm = (noteId) => {
     // Give this component access to our application's notes state
     const allNotes = useNotes();
 
@@ -46,4 +48,44 @@ export const NoteEditForm = (noteId) => {
   </div>
     </div>
     `
+} */
+
+export const NoteEditForm = (noteId) => {
+  getCriminals()
+  .then(getNotes)
+    .then(() => {
+        const notesArray = useNotes();
+        const criminalsArray = useCriminals();
+      
+            const noteWeWantToEdit = notesArray.find(singleNote=> singleNote.id === noteId)
+            // const relatedCriminal = criminalsArray.find(criminal => criminal.id === noteWeWantToEdit.id)
+            contentTarget.innerHTML = `
+            <div class="edit-form row g-3 text-white bg-secondary"> 
+            <h2>Edit Note</h2>     
+      <div class="col-md-12">
+        <label for="noteDate" class="form-label">Date</label>
+        <input type="date" name="noteDate" class="form-control" id="note-date" value="${noteWeWantToEdit.date}">
+      </div>
+      <div class="col-md-12">
+        <label for="noteSuspect" class="form-label">Suspect In Question:</label>
+        <select name="noteSuspect" class="criminalSelect form-control" id="note-suspect">
+        <option value="${noteWeWantToEdit.criminalId}">Select a criminal...</option>
+        ${
+          criminalsArray.map((criminal) => {
+              return `<option value="${criminal.id}">${criminal.name}</option>`
+          })
+        }
+        </select> 
+      </div>
+      <div class="col-12">
+        <label for="noteText" class="form-label">Notes:</label>
+        <input type="text" class="form-control" name="noteText" value="${noteWeWantToEdit.text}" id="note-text">
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-light btn-outline-dark" id="saveNote-${noteWeWantToEdit.id}">
+        <i class="bi bi-journal-check"></i>Save Note</button>
+      </div>
+        </div>
+          `
+    })
 }
